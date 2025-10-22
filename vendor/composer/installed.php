bce8a3f87d68eb4c1d7242ebb71bb9eb741ea608
GeorgeMcK <george@hookstream.com>
2025-10-22 10:07:59 -0700

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-plover/plover-theme',
+        'pretty_version' => 'v1.3.5',
+        'version' => '1.3.5.0',
+        'reference' => '27588c3b7d536aac0b16cc924c003e2809741ee6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-plover/plover-theme' => array(
+            'pretty_version' => 'v1.3.5',
+            'version' => '1.3.5.0',
+            'reference' => '27588c3b7d536aac0b16cc924c003e2809741ee6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
